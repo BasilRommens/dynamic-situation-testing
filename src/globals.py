c148d88d6755f7c2be1a106206e774bcc18cb99a
basil <basil.rommens@student.uantwerpen.be>
2023-03-09 20:45:22 +0100

    a globals file containing everything that is global

---
+++ src/globals.py
@@ -0,0 +1,8 @@
+# color-blind friendly color values
+colors = [(0, 0, 0), (0.9, 0.62, 0.0), (.34, .71, .91), (.0, .62, .45),
+          (.94, .89, .26), (.0, .45, .7), (.83, .37, .0), (.8, .47, .65)]
+
+# color-blind friendly color values as strings
+colors_string = ['rgb(0,0,0)', 'rgb(230,159,0)', 'rgb(86,180,233)',
+                 'rgb(0,158,115)', 'rgb(240,228,66)', 'rgb(0,114,178)',
+                 'rgb(212,94,0)', 'rgb(204,119,102)']
