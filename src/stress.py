c2247acb4beb608fdc796a1bd762796a996fc55b
basil <basil.rommens@student.uantwerpen.be>
2023-03-09 20:52:51 +0100

    added stress file for measuring stress in final dimensionality reduction

---
+++ src/stress.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+if __name__ == '__main__':
+    pass
+
+
+def stress(M1, M2):
+    numerator = sum(np.power(M1[i, j] - M2[i, j], 2)
+                    for j in range(M1.shape[1])
+                    for i in range(M1.shape[0]))
+    denominator = sum(np.power(M1[i, j], 2)
+                      for j in range(M1.shape[1])
+                      for i in range(M1.shape[0]))
+    return np.sqrt(numerator / denominator)
+
+
+def total_stress(og_mat, dimred_mat, width_DD_mat, height_DD_mat, beta_DD=1 / 7,
+                 beta_VV=4 / 7, beta_DV=1 / 7, beta_VD=1 / 7):
+    # determine the distances from the original distance matrix
+    orig_DD = og_mat[:height_DD_mat, :width_DD_mat]
+    orig_VD = og_mat[height_DD_mat:, :width_DD_mat]
+    orig_DV = og_mat[:height_DD_mat, width_DD_mat:]
+    orig_VV = og_mat[height_DD_mat:, width_DD_mat:]
+
+    # determine the distances from the dimensionality reduced distance matrix
+    DD = dimred_mat[:height_DD_mat, :width_DD_mat]
+    VD = dimred_mat[height_DD_mat:, :width_DD_mat]
+    DV = dimred_mat[:height_DD_mat, width_DD_mat:]
+    VV = dimred_mat[height_DD_mat:, width_DD_mat:]
+
+    # determine the stress for each submatrix and the total stress
+    E_DD = stress(orig_DD, DD)
+    E_VD = stress(orig_VD, VD)
+    E_DV = stress(orig_DV, DV)
+    E_VV = stress(orig_VV, VV)
+    E_A = beta_DD * E_DD + beta_DV * E_DV + beta_VD * E_VD + beta_VV * E_VV
+
+    # print all the stress values
+    print(f'{E_DD=}')
+    print(f'{E_VD=}')
+    print(f'{E_DV=}')
+    print(f'{E_VV=}')
+    print(f'{E_A=}')
+
+    return E_DD, E_VD, E_DV, E_VV, E_A
