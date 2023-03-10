9dc78ad8a10f172d2aae629f0ed7635607c5c1da
Basil Rommens <basil.rommens@student.uantwerpen.be>
2023-03-10 19:17:52 +0100

    added table to the index page

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
+import numpy as np
 from flask import Flask, render_template, redirect, url_for
+
+from inout import read_data
+from knn import calc_dist_mat, knn_situation
+from process import process_all
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
@@ -11,9 +16,48 @@ app = Flask(__name__, template_folder='templates')
 # which tells the application which URL should call
 # the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def home():
-    return render_template('home.html')
+    headers = sorted(list(tuples.columns))
+    headers.insert(0, 'Score')
+
+    new_tuples = list()
+    # create a new tuple with the score and the k nearest neighbors along with
+    # its own values
+    for tuple_idx, score, k_prot, k_unprot in valid_tuples:
+        # sort the tuple by the attribute name and then only take the values
+        new_tuple = list(map(lambda x: x[1], sorted(
+            list(tuples.iloc[tuple_idx].to_dict().items()),
+            key=lambda x: x[0])))
+        # insert the score at the beginning of the tuple
+        new_tuple.insert(0, score)
+
+        # sort the tuple by the attribute name and then only take the values
+        # but now for the k nearest neighbors of the protected group
+        new_k_prot = list()
+        for prot_idx, _ in k_prot:
+            new_prot_el = list(map(lambda x: x[1], sorted(
+                list(tuples.iloc[prot_idx].to_dict().items()),
+                key=lambda x: x[0])))
+            new_prot_el.insert(0, 'protected')
+            new_k_prot.append(new_prot_el)
+
+        # sort the tuple by the attribute name and then only take the values
+        # but now for the k nearest neighbors of the unprotected group
+        new_k_unprot = list()
+        for unprot_idx, _ in k_unprot:
+            new_unprot_el = list(map(lambda x: x[1], sorted(
+                list(tuples.iloc[unprot_idx].to_dict().items()),
+                key=lambda x: x[0])))
+            new_unprot_el.insert(0, 'unprotected')
+            new_k_unprot.append(new_unprot_el)
+
+        # create a triple tuple
+        new_tuple = (new_tuple, new_k_prot, new_k_unprot)
+
+        # add the new tuple
+        new_tuples.append(new_tuple)
+
+    return render_template('home.html', items=new_tuples, headers=headers)
 
 
 @app.route('/explore')
@@ -30,6 +74,28 @@ def discriminate():
 def upload():
     return render_template('upload.html')
 
+
+with app.app_context():
+    # read the data from the csv and json file
+    r = read_data('data/german_credit_data.json',
+                  'data/german_credit_data_class.csv')
+
+    # process the data
+    tuples, ranked_values, decision_attribute = process_all(r)
+    # determine the distances
+    protected_attributes = {"Sex": ["female"]}
+    dist_mat = calc_dist_mat(tuples, ranked_values, r.attribute_types,
+                             decision_attribute,
+                             protected_attributes)
+    # write dump
+    dist_mat.dump('data/dist_mat.dump')
+    # read the same dump
+    dist_mat = np.load('data/dist_mat.dump', allow_pickle=True)
+
+    # apply the situation testing algorithm with knn
+    k = 4
+    valid_tuples = knn_situation(k, tuples, dist_mat, protected_attributes,
+                                 decision_attribute)
 
 # main driver function
 if __name__ == '__main__':
