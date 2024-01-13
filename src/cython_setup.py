302ba7195284fbba2fcb322c681d734c79c87ac3
basil <basil.rommens@student.uantwerpen.be>
2024-01-13 20:30:29 +0100

    removed the distools (deprecated) requirement and changed it to setuptools. Should run with Python 3.12 now

--- src/cython_setup.py
+++ src/cython_setup.py
@@ -1,8 +1,7 @@
-from distutils.core import Extension, setup
+from setuptools import setup
 from Cython.Build import cythonize
 
 # define an extension that will be cythonized and compiled
-ext = Extension(name="distance_fast", sources=["distance_fast.pyx"],
-                extra_compile_args=['-O3', '-ffast-math', '-march=native'],
-                language="c++")
-setup(ext_modules=cythonize(ext))
+ext = setup(name="distance_fast",
+            extra_compile_args=['-O3', '-ffast-math', '-march=native'],
+            language="c++", ext_modules=cythonize("distance_fast.pyx"))
